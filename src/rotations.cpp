aeda623acc27c6a01671c2c7351a44c747d2fb25
xnyGitHub <32583691+xnyGitHub@users.noreply.github.com>
2022-04-23 15:40:23 +0100

    added horizontal and vertical rotations

--- src/rotations.cpp
+++ src/rotations.cpp
@@ -1,8 +1,25 @@
 # include "../src/rotations.h"
 
-U64 Rotate::flip_vertically(U64 bitboard) {return 0ULL;};
+U64 Rotate::flip_vertically(U64 bitboard) {
+    const U64 k1 = 0x00FF00FF00FF00FF;
+    const U64 k2 = 0x0000FFFF0000FFFF;
 
-U64 Rotate::flip_horizontally(U64 bitboard) {return 0ULL;};
+    bitboard = ((bitboard >>  8) & k1) | ((bitboard & k1) <<  8);
+    bitboard = ((bitboard >> 16) & k2) | ((bitboard & k2) << 16);
+    bitboard = ( bitboard >> 32)       | ( bitboard       << 32);
+    return bitboard;
+};
+
+U64 Rotate::flip_horizontally(U64 bitboard) {
+    const U64 k1 = 0x5555555555555555;
+    const U64 k2 = 0x3333333333333333;
+    const U64 k4 = 0x0f0f0f0f0f0f0f0f;
+
+    bitboard = ((bitboard >> 1) & k1) | ((bitboard & k1) << 1);
+    bitboard = ((bitboard >> 2) & k2) | ((bitboard & k2) << 2);
+    bitboard = ((bitboard >> 4) & k4) | ((bitboard & k4) << 4);
+    return bitboard;
+};
 
 U64 Rotate::flip_diagonally_A1H8(unsigned long long bitboard)  {return 0ULL;};
 
