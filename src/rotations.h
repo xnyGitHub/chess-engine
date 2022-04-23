089e70d4e63fb118e3fb3fe5c7ffb1c5195efba1
xnyGitHub <32583691+xnyGitHub@users.noreply.github.com>
2022-04-23 15:21:49 +0100

    created header file for rotations

---
+++ src/rotations.h
@@ -0,0 +1,14 @@
+#ifndef TEST_PROJECT_ROTATIONS_H
+#define TEST_PROJECT_ROTATIONS_H
+#define U64 unsigned long long
+
+class Rotate {
+public:
+    U64 flip_vertically;
+    U64 flip_horizontally;
+    U64 flip_diagonally_A1H8;
+    U64 flip_diagonally_A8H1;
+    U64 rotate_left;
+    U64 rotate_right;
+};
+#endif //TEST_PROJECT_ROTATIONS_H
