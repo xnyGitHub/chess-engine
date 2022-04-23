92b8cb5fb1be07aa93caa23ab3c6a694a74fba96
xnyGitHub <32583691+xnyGitHub@users.noreply.github.com>
2022-04-23 15:33:42 +0100

    added skeleton implementation

---
+++ src/rotations.cpp
@@ -0,0 +1,13 @@
+# include "../src/rotations.h"
+
+U64 Rotate::flip_vertically(U64 bitboard) {return 0ULL;};
+
+U64 Rotate::flip_horizontally(U64 bitboard) {return 0ULL;};
+
+U64 Rotate::flip_diagonally_A1H8(unsigned long long bitboard)  {return 0ULL;};
+
+U64 Rotate::flip_diagonally_A8H1(unsigned long long bitboard) {return 0ULL;};
+
+U64 Rotate::rotate_left(unsigned long long bitboard) {return 0ULL;};
+
+U64 Rotate::rotate_right(unsigned long long bitboard) {return 0ULL;};
