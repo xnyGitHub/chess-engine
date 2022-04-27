acfbd35c46e91db4f07d9d7fe64ae6888ab2e647
Michael <michaelobineche99@gmail.com>
2022-04-27 13:15:30 +0100

    added flags & comments, move now represented in bits

--- src/move.cpp
+++ src/move.cpp
@@ -1,1 +1,34 @@
+// Move implementation file
 #include "move.h"
+
+const char Move::RANKS[] = {'1', '2', '3', '4', '5', '6', '7', '8'};
+const char Move::FILES[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+
+Move::Move(unsigned int start, unsigned int stop, unsigned int flags) {
+    // Each move is represented using 16-bits
+    // The first 6-bits(values 0-63) are for representing the start index
+    // The next 6-bits(values 0-63) are for representing the end index
+    // The last 4-bits(values 0-15) are for flags for the move. e.g Castle, Capture, Promotion etc.
+    _move = ((flags & 0xf)<<12) | ((start & 0x3f)<<6) | (stop & 0x3f);
+}
+
+unsigned int Move::get_start() {
+    // Return the start move index
+    return (_move >> 6) & 0x3f;
+}
+
+unsigned int Move::get_stop() {
+    // Return the end move index
+    return _move & 0x3f;
+}
+
+unsigned int Move::get_flags() {
+    // Return the move flag
+    return (_move >> 12) & 0x0f;
+}
+
+std::string Move::get_string_move() {
+    // Return a string representation of the move
+    // To-do
+    return "Not implemented";
+}
--- src/move.h
+++ src/move.h
@@ -2,20 +2,41 @@
 #define MOVE_H
 #include <string>
 
-class move {
+class Move {
 public:
-    move(int start, int stop, int type);
+    // Constructor
+    Move(unsigned int start, unsigned int stop, unsigned int flags);
+
+    // Getters
+    unsigned int get_start();
+    unsigned int get_stop();
+    unsigned int get_flags();
     std::string get_string_move();
+
+    // Flags to identify move type
     enum flag {
-        normal,
-        capture,
-        castle
+        quiet,
+        double_pawn_push,
+        king_castle,
+        queen_castle,
+        captures,
+        ep_capture,
+        knight_promotion,
+        bishop_promotion,
+        rook_promotion,
+        queen_promotion,
+        knight_promotion_capture,
+        bishop_promotion_capture,
+        rook_promotion_capture,
+        queen_promotion_capture,
     };
 private:
-    int _move;
+    // The move
+    unsigned int _move;
 
-
+    // Constants to convert move bits to string
+    const static char RANKS[8];
+    const static char FILES[8];
 };
-
 
 #endif //MOVE_H
